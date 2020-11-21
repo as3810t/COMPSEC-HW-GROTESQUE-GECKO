3e181bf0d319427e3d106319cfdc537eb98bab5e
Zsako Agnes <zs.agi27@gmail.com>
2020-11-21 20:05:53 +0100

    Add function to NetworkDataSource which gives back a caff list

--- client/app/src/main/java/com/example/grotesquegecko/domain/interactors/CaffInterctor.kt
+++ client/app/src/main/java/com/example/grotesquegecko/domain/interactors/CaffInterctor.kt
@@ -1,22 +1,13 @@
 package com.example.grotesquegecko.domain.interactors
 
+import com.example.grotesquegecko.data.network.NetworkDataSource
 import com.example.grotesquegecko.ui.caffsearcher.models.CaffPreview
 import javax.inject.Inject
 
-class CaffInterctor @Inject constructor() {
+class CaffInterctor @Inject constructor(
+    private val networkDataSource: NetworkDataSource
+) {
     suspend fun getCaffList(): MutableList<CaffPreview> {
-        //TODO get data from network data source
-        return mutableListOf(
-            CaffPreview(
-                "1",
-                "Grotesque",
-                "https://scx2.b-cdn.net/gfx/news/2017/2-5-universityof.jpg"
-            ),
-            CaffPreview(
-                "2",
-                "Gecko",
-                "https://i.insider.com/53bdb318ecad04c707b61b17?width=700&format=jpeg&auto=webp"
-            )
-        )
+        return networkDataSource.getCaffList()
     }
 }
