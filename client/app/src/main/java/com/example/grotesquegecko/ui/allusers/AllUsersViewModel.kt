7e31b5bf4a31d9c9330a02cbef5ecd8272a63a21
Zsako Agnes <zs.agi27@gmail.com>
2020-12-06 23:17:56 +0100

    Fix all users list

--- client/app/src/main/java/com/example/grotesquegecko/ui/allusers/AllUsersViewModel.kt
+++ client/app/src/main/java/com/example/grotesquegecko/ui/allusers/AllUsersViewModel.kt
@@ -1,14 +1,21 @@
 package com.example.grotesquegecko.ui.allusers
 
+import co.zsmb.rainbowcake.base.OneShotEvent
 import co.zsmb.rainbowcake.base.RainbowCakeViewModel
+import com.example.grotesquegecko.data.network.models.UserData
 import javax.inject.Inject
 
 class AllUsersViewModel @Inject constructor(
     private val allUsersPresenter: AllUsersPresenter
 ) : RainbowCakeViewModel<AllUsersViewState>(Loading) {
 
+    data class UserDatas(
+        val users: MutableList<UserData>
+    ) : OneShotEvent
+
     fun load() = execute {
-        viewState = AllUsersReady(allUsersPresenter.getAllUsers())
+        postEvent(UserDatas(allUsersPresenter.getAllUsers()))
+        viewState = AllUsersReady
     }
 
     fun editUserData(email: String, password: String, username: String, id: String) = execute {
--- client/app/src/main/java/com/example/grotesquegecko/ui/allusers/AllUsersViewState.kt
+++ client/app/src/main/java/com/example/grotesquegecko/ui/allusers/AllUsersViewState.kt
@@ -1,9 +1,7 @@
 package com.example.grotesquegecko.ui.allusers
 
-import com.example.grotesquegecko.data.network.models.UserData
-
 sealed class AllUsersViewState
 
 object Loading : AllUsersViewState()
 
-data class AllUsersReady(val allUsers: MutableList<UserData>) : AllUsersViewState()
+object AllUsersReady : AllUsersViewState()
